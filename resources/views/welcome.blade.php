11d130fb8007a36ee547d9066eb44addeb390b5d
Dip Ghosh <dip@storat.com>
2023-01-30 23:29:47 +0600

    chnages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,66 +11,83 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
             crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
 
 </head>
 <body class="antialiased">
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    <div class="max-w-6xl mx-auto sm:px-6 lg:py-8">
-            <div class="row d-flex justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-5">
-                    <div class="card rounded-3">
-                        <div class="card-body p-4">
-                            <div class="text-center mb-4">
-                                <h6>Payment Through Adyen</h6>
+<div class="container align-self-center mt-5">
+    <div class="row ">
+        <div class="col-md-4"></div>
+        <div class="col-md-4" >
+            <div class="card">
+                <div class="col align-self-center mt-5">
+                    <div class='col align-self-center'></div>
+                    <div class='col align-self-center'>
+                        <form name="payment-form" action="/" class="require-validation" id="payment-form" method="post">
+
+                            <div class='row'>
+                                <div class='col-xs-12 form-group required'>
+                                    <label class='control-label'>Card Holder Name</label>
+                                    <input class='form-control' name="card_holder" id="card-holder" value=""  type='text'>
+                                    <p class="error name-error text-danger"></p>
+                                </div>
                             </div>
-                            <hr>
-                            <form action="{{route('api.payment')}}" method="POST" >
-                                @csrf
-                                <div class="row">
-                                    <div class="col-md-6 mb-4 pb-1">
-                                        <label class="form-label" for="formControlLgXc">Card Number</label>
-                                        <input type="text" name="card_number" id="formControlLgXc" placeholder="4111 1111 4555 1142" class="form-control form-control-lg" value=""/>
-
-                                    </div>
-                                    <div class="col-md-6 mb-4 pb-1">
-                                        <label class="form-label" for="formControlLgXsd">Cardholder's Name</label>
-                                        <input type="text" name="card_holder" id="formControlLgXsd" class="form-control form-control-lg"  placeholder="Dip Ghosh" value=""/>
-
-                                    </div>
-
-                                    <div class="col-md-6 mb-4 pb-1">
-                                        <div class="form-outline">
-                                            <label class="form-label" for="formControlLgExpk">Expire</label>
-                                            <input name="expire" type="password" id="formControlLgExpk"
-                                                   class="form-control form-control-lg" value=""
-                                                   placeholder="MM/YYYY"/>
-
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6 mb-4 pb-1">
-                                        <div class="form-outline">
-                                            <label class="form-label" for="formControlLgcvv">CVV</label>
-                                            <input  name="cvv" type="password" id="formControlLgcvv"  value=""
-                                                    class="form-control form-control-lg"
-                                                    placeholder="737"/>
-
-                                        </div>
-                                    </div>
+                            <br>
+                            <div class='row'>
+                                <div class='col-xs-12 form-group required'>
+                                    <label class='control-label'> Card Number <span class="text-danger">*</span></label>
+                                    <input class='form-control' size='4' type='text'>
                                 </div>
-                                <div class="row">
-                                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                        <button type="submit" class="btn btn-primary ">Pay Now</button>
-                                    </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class='col-xs-12 form-group required'>
+                                    <label class='control-label'> Currency <span class="text-danger">*</span></label>
+                                <select class="form-select" aria-label="Default select example">
+
+                                    <option selected>Select Currency</option>
+                                    <option value="USD">USD</option>
+                                    <option value="EURO">EURO</option>
+                                    <option value="GBP">GBP</option>
+                                </select>
+                                </div>
+                            </div>
+                            <br>
+                            <div class='row'>
+                                <div class='col-xs-12 form-group required'>
+                                    <label class='control-label'> Amount <span class="text-danger">*</span></label>
+                                    <input class='form-control' type='number' min="1">
+                                </div>
+                            </div>
+
+                            <br>
+                            <div class='row'>
+                                <div class='col-md-6 form-group cvc required'>
+                                    <label class='control-label'>CVC  <span class="text-danger">*</span></label>
+                                    <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text'>
+                                </div>
+                                <div class='col-md-6 form-group expiration required'>
+                                    <label class='control-label'>Expiration  <span class="text-danger">*</span></label>
+                                    <input class='form-control card-expiry-month' placeholder='MM//YYYY' size='6' type='text'>
                                 </div>
 
-                            </form>
-                        </div>
+                            </div>
+                            <br>
+                            <div class='form-row mb-5'>
+                                <div class='col-md-12 form-group'>
+                                    <button  id="submit-btn" class='form-control btn btn-primary submit-button' type='submit'>Pay »</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
+                    <div class='col align-self-center'></div>
                 </div>
             </div>
         </div>
+        <div class="col-md-4"></div>
+    </div>
 </div>
-
 
 </body>
 </html>
